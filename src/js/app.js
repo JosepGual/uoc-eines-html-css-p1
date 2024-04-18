283db11549e9bfc946bffbd77c3a900b230c2fcc
JosepGual <josep.gualr@gmail.com>
2024-04-18 21:26:37 +0200

    use grid css, home page

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,1 @@
-import '@fortawesome/fontawesome-free/css/all.css'
-import {toggleMenu }  from './toggle-menu.js';
-
-function toggleMenu(){
-    var menu = document.getElementById('mobile-menu');
-    menu.classList.toggle('d-none')
-}
-
-export {toggleMenu}
+import "@fortawesome/fontawesome-free/css/all.css";
--- src/js/toggle-menu.js
+++
@@ -1,6 +0,0 @@
-const toggleMenu = () => {
-    var menu = document.getElementById('mobile-menu');
-    menu.classList.toggle('d-none')
-}
-
-export {toggleMenu}
