7f881f01ccea46ef65f99b0c60fbcb9402324887
JosepGual <josep.gualr@gmail.com>
2024-06-19 17:35:43 +0200

    toogleMenu script

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
 import "@fortawesome/fontawesome-free/css/all.css";
+
+
+
+document.getElementById("demo").addEventListener("click", toogleMenu);
+var menu = document.getElementById('mobile-menu')
+
+function toogleMenu(){
+    menu.classList.toggle('d-none')
+}
