127467fb398e680745107159935c663358714ccb
JosepGual <josep.gualr@gmail.com>
2024-06-20 17:38:20 +0200

    multi iframes

--- src/js/spotify.js
+++ src/js/spotify.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   window.onSpotifyIframeApiReady = (IFrameAPI) => {
-    const element = document.getElementById("embed-iframe");
-    const options = {
-      uri: "spotify:track:5tn5NYeF8c8A2VeIa53kcv",
-    };
-    const callback = (EmbedController) => {};
-    IFrameAPI.createController(element, options, callback);
+    const elements = document.querySelectorAll(".spotify-iframe");
+    elements.forEach((element) => {
+      const uri = element.getAttribute("data-uri");
+      const options = {
+        uri: uri,
+      };
+      const callback = (EmbedController) => {};
+      IFrameAPI.createController(element, options, callback);
+    });
   };
 });
