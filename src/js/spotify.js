9358c6a504477fbed7bfff83a6811eb0616afad2
JosepGual <josep.gualr@gmail.com>
2024-06-21 14:12:52 +0200

    heig ajust

--- src/js/spotify.js
+++ src/js/spotify.js
@@ -4,7 +4,7 @@ window.onSpotifyIframeApiReady = (IFrameAPI) => {
     const uri = element.getAttribute("data-uri");
     const options = {
       uri: uri,
-      height: 230,
+      height: 232,
     };
     const callback = (EmbedController) => {
       EmbedController.togglePlay();
